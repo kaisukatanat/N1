eba9752383c878f97814996c4f7a7a50a6860690
LeoBaoNguyen12 <giabao07092004@gmail.com>
2024-12-07 14:40:07 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,4 +5,6 @@ def tongchuso(n):
         return n%10+tongchuso(n//10)
 n=int(input("nhap n "))
 print("tong chu so", tongchuso(n))
-    
+df=pd.read_csv("Life expectancy.csv")
+fig=px.line(df,x="Year",y="life expectancy")
+fig.show()
