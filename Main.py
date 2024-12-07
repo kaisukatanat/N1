d9d417d0aa30f5d6968919bf4cd573ea66787aa5
nguyễn gia bảo <nguyengiabao@MacBook-Air-cua-nguyen.local>
2024-12-07 18:26:22 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,1 @@
-def tongchuso(n):
-    if(n==0):
-        return 0 # diemdung
-    else:
-        return n%10+tongchuso(n//10)
-n=int(input("nhap n "))
-print("tong chu so", tongchuso(n))
-df=pd.read_csv("Life expectancy.csv")
-fig=px.line(df,x="Year",y="life expectancy")
-fig.show()
+print("Hello")
